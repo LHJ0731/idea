24ed85ac4dd603796ed277c22dad6303935ebb66
LHJ0731 <1397968566@qq.com>
2020-05-13 10:59:20 +0800

    无

--- src/Helloworld/Helloworld.java
+++ src/Helloworld/Helloworld.java
@@ -4,6 +4,5 @@ public class Helloworld {
     public static void main(String args[]){
         System.out.println("Helloworld");
         System.out.println("Nice to meet you1");
-        System.out.println("Nice");
     }
 }
