754c88dbd46ef15932663979ada576c00c78c7f4
LHJ0731 <1397968566@qq.com>
2020-05-13 10:51:21 +0800

    增加多一行代码

--- src/Helloworld/Helloworld.java
+++ src/Helloworld/Helloworld.java
@@ -4,5 +4,6 @@ public class Helloworld {
     public static void main(String args[]){
         System.out.println("Helloworld");
         System.out.println("Nice to meet you1");
+        System.out.println("Nice");
     }
 }
